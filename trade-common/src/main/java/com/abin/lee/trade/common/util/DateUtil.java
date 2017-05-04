968cf1596b1fcdcf5d77ed14153a9bdf51a8b0e5
zondahuman <zondahuman@gmail.com>
2017-05-04 13:39:51 +0800

    add

--- trade-common/src/main/java/com/abin/lee/trade/common/util/DateUtil.java
+++ trade-common/src/main/java/com/abin/lee/trade/common/util/DateUtil.java
@@ -26,7 +26,7 @@ public class DateUtil {
     public static Date getYMDTime(String param) {
         Date result = null;
         try {
-            result = DateFormatUtils.ISO_DATE_FORMAT.parse(param);
+            result = DateFormatUtils.ISO_8601_EXTENDED_DATE_FORMAT.parse(param);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -46,7 +46,7 @@ public class DateUtil {
     public static Date getHMSTime(String param) {
         Date result = null;
         try {
-            result = DateFormatUtils.ISO_TIME_NO_T_FORMAT.parse(param);
+            result = DateFormatUtils.ISO_8601_EXTENDED_TIME_FORMAT.parse(param);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -116,18 +116,93 @@ public class DateUtil {
     }
 
 
+    public static String getYMDHMSTime(long date) {
+        String result = "";
+        try {
+            result = DateFormatUtils.format(date, ymdhmsSdf);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
 
-//    public static void main(String[] args) {
-//        String result = getYMDHMSTime();
-//        System.out.println("result="+result);
-//        String result1 = getYMDTime();
-//        System.out.println("result1="+result1);
-//        String result2 = getHMSTime();
-//        System.out.println("result2="+result2);
-//        Date result3 = getYMDTime("1999-02-15");
-//        System.out.println("result3="+result3);
-//        Date result4 = getHMSTime("12:00:13");
-//        System.out.println("result4="+result4);
-//    }
+
+    public static String getYMDTime(long date) {
+        String result = "";
+        try {
+            result = DateFormatUtils.format(date, ymdSdf);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+
+    public static String getHMSTime(long date) {
+        String result = "";
+        try {
+            result = DateFormatUtils.format(date, ymdhmsSdf);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static Long getYMDTimeStamp() {
+        Long result = null;
+        try {
+            result = DateFormatUtils.ISO_8601_EXTENDED_DATE_FORMAT.parse(getYMDTime(new Date())).getTime();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static Long getYMDHMSTimeStamp() {
+        Long result = null;
+        try {
+            result = DateFormatUtils.ISO_8601_EXTENDED_DATE_FORMAT.parse(getYMDHMSTime(new Date())).getTime();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static Date getYMDDate(Long timestamp) {
+        Date result = null;
+        try {
+            result = new Date(timestamp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String result = getYMDHMSTime();
+        System.out.println("result="+result);
+        String result1 = getYMDTime();
+        System.out.println("result1="+result1);
+        String result2 = getHMSTime();
+        System.out.println("result2="+result2);
+        Date result3 = getYMDTime("1999-02-15");
+        System.out.println("result3="+result3);
+        Date result4 = getHMSTime("12:00:13");
+        System.out.println("result4="+result4);
+        String result5 = getYMDHMSTime(1487838953000L);
+        System.out.println("result5="+result5);
+        Long result6 = getYMDTimeStamp();
+        System.out.println("result6="+result6);
+        Long result7 = getYMDHMSTimeStamp();
+        System.out.println("result7="+result7);
+
+        Date result8 = getYMDDate(System.currentTimeMillis());
+        System.out.println("result8="+result8);
+
+
+    }
 
 }
